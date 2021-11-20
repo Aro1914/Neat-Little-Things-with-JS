4c73b0d5eec879923e6687f7f07b26d30a81f4af
Emmanuel <agbavweisaac@gmail.com>
2021-11-20 11:45:03 +0100

    Touched up countBs.js

--- countBs.js
+++ countBs.js
@@ -1,6 +1,8 @@
+// The commented block of code below, defines a function that takes in a string and returns the number of times the alphabet B occurs in it.
+
 // let countBs = (input) => {
 //     let count = 0;
-//     for (const _char of input) {
+//     for (const _char of input.toUpperCase()) {
 //         if (_char === 'B') {
 //             count++;
 //         }
@@ -10,14 +12,16 @@
 
 // countBs('Beans and bitter leaf soup');
 
+// The below block of code counts and returns the number of times any character passed as the second argument appears in the first string argument
+
 let countBs = (input, char) => {
     let count = 0;
-    for (const _char of input) {
-        if (_char === char) {
+    for (const _char of String(input).toUpperCase()) {
+        if (_char === String(char).toUpperCase()) {
             count++;
         }
     }
     console.log(count);
 }
 
-countBs('Beans and bitter leaf soup', ' ');
+countBs('b1b2b3b4b5b1b2b2b3b3b3', 'b');
