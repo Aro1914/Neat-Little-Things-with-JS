3f3b3d0f81866c3bf63723e8f6f2eba78c2e7a44
Emmanuel <agbavweisaac@gmail.com>
2022-02-01 23:00:15 +0100

    Updated Other_Cool_Stuff files

--- Other_Cool_Stuff/Color_Changer/color_changer.js
+++ Other_Cool_Stuff/Color_Changer/color_changer.js
@@ -1,5 +1,25 @@
-let btn  = document.querySelector('button');
+let btn = document.querySelector('button');
+
+const r = () => {
+    return Math.floor(Math.random() * 256);
+}
+
+const g = () => {
+    return Math.floor(Math.random() * 256);
+}
+
+const b = () => {
+    return Math.floor(Math.random() * 256);
+}
 
 btn.addEventListener('click', () => {
-    document.querySelector('body').classList.toggle("newBodyBg");
+    let red = r();
+    let redCompliment = 255 - red;
+    let green = g();
+    let greenCompliment = 255 - green;
+    let blue = b();
+    let blueCompliment = 255 - blue;
+    document.querySelector('body').style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    btn.style.backgroundColor = `rgb(${redCompliment}, ${greenCompliment}, ${blueCompliment})`;
+    btn.style.color = `rgb(${red}, ${green}, ${blue})`;
 });
