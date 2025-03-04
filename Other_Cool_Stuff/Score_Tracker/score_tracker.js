4069160a035df9d26a52813ac6228593c904e6ad
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2025-03-04 01:04:29 +0100

    Refactor score_tracker.js for improved readability and consistency

--- Other_Cool_Stuff/Score_Tracker/score_tracker.js
+++ Other_Cool_Stuff/Score_Tracker/score_tracker.js
@@ -1,88 +1,97 @@
-let score1 = document.querySelector('#p1Score');
-let score2 = document.querySelector('#p2Score');
+let score1 = document.querySelector('#p1Score')
+let score2 = document.querySelector('#p2Score')
 
-let games = document.querySelector('#games');
+let games = document.querySelector('#games')
 
-let input = document.querySelector('#numInput');
+let input = document.querySelector('#numInput')
 
-let p1btn = document.querySelector('#p1btn');
-let p2btn = document.querySelector('#p2btn');
+let p1btn = document.querySelector('#p1btn')
+let p2btn = document.querySelector('#p2btn')
 
-let reset = document.querySelector('#reset');
+let reset = document.querySelector('#reset')
 
 input.addEventListener('change', () => {
-    games.innerHTML = input.value;
-    gameOver();
-});
+	games.innerHTML = input.value
+	gameOver()
+})
 
 p1btn.addEventListener('click', () => {
-    increase(true);
-});
+	increase(true)
+})
 
 p2btn.addEventListener('click', () => {
-    increase(false);
-});
+	increase(false)
+})
 
 reset.addEventListener('click', () => {
-    resetAll();
-}); 
+	resetAll()
+})
 
 const increase = (param) => {
-    let score;
-    if (param) {
-        score = Number(score1.textContent);
-        score++;
-        score1.innerHTML = score;
-        gameOver();
-    } else {
-        score = Number(score2.textContent);
-        score++;
-        score2.innerHTML = score;
-        gameOver();
-    }
+	let score
+	if (param) {
+		score = Number(score1.textContent)
+		score++
+		score1.innerHTML = score
+		gameOver()
+	} else {
+		score = Number(score2.textContent)
+		score++
+		score2.innerHTML = score
+		gameOver()
+	}
 }
 
-const scores = document.querySelectorAll('h2 span');
+const scores = document.querySelectorAll('h2 span')
 
 const gameOver = () => {
-    scores.forEach(element => {
-        element.classList.remove('winner');
-        element.classList.remove('draw');
-    });
+	scores.forEach((element) => {
+		element.classList.remove('winner')
+		element.classList.remove('draw')
+	})
 
-    const gamesPlayed = Number(games.textContent);
-    const totalScore = Number(score1.innerHTML) + Number(score2.innerHTML);
-    if (gamesPlayed === totalScore) {
-        p1btn.disabled = true;
-        p2btn.disabled = true;
-    } else if (p1btn.disabled === true && gamesPlayed > totalScore) {
-        p1btn.disabled = false;
-        p2btn.disabled = false;
-    } else if (gamesPlayed < totalScore) {
-        resetAll();
-    }
+	const gamesPlayed = Number(games.textContent)
+	const totalScore = Number(score1.innerHTML) + Number(score2.innerHTML)
+	const winningScore = Math.max(
+		Number(score1.innerHTML),
+		Number(score2.innerHTML)
+	)
+	const losingScore = Math.min(
+		Number(score1.innerHTML),
+		Number(score2.innerHTML)
+	)
+	const gamesLeft = gamesPlayed - totalScore
+	const canStillPlay = winningScore - losingScore <= gamesLeft
+	if (gamesPlayed === totalScore || !canStillPlay) {
+		p1btn.disabled = true
+		p2btn.disabled = true
+	} else if (p1btn.disabled === true && gamesPlayed > totalScore) {
+		p1btn.disabled = false
+		p2btn.disabled = false
+	} else if (gamesPlayed < totalScore) {
+		resetAll()
+	}
 
-    const s1 = Number(score1.innerHTML);
-    const s2 = Number(score2.innerHTML);
-    if (gamesPlayed === totalScore && s1 > s2) {
-        score1.classList.add('winner');
-    } else if (gamesPlayed === totalScore && s1 < s2) {
-        score2.classList.add('winner');
-    } else if (gamesPlayed === totalScore) {
-        scores.forEach(element => {
-            element.classList.add('draw');
-        });
-    }
+	const s1 = Number(score1.innerHTML)
+	const s2 = Number(score2.innerHTML)
+	if ((gamesPlayed === totalScore || !canStillPlay) && s1 > s2) {
+		score1.classList.add('winner')
+	} else if ((gamesPlayed === totalScore || !canStillPlay) && s1 < s2) {
+		score2.classList.add('winner')
+	} else if (gamesPlayed === totalScore || !canStillPlay) {
+		scores.forEach((element) => {
+			element.classList.add('draw')
+		})
+	}
 }
 
 const resetAll = () => {
-    score1.innerHTML = '0';
-    score2.innerHTML = '0';
-    p1btn.disabled = false;
-    p2btn.disabled = false;
-    scores.forEach(element => {
-        element.classList.remove('winner');
-        element.classList.remove('draw');
-    });
+	score1.innerHTML = '0'
+	score2.innerHTML = '0'
+	p1btn.disabled = false
+	p2btn.disabled = false
+	scores.forEach((element) => {
+		element.classList.remove('winner')
+		element.classList.remove('draw')
+	})
 }
-
